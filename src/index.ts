aada434a3e5781666315803152596fb3fd1b99b7
fahmifan <me@fahmifan.id>
2024-03-06 18:10:06 +0700

    feat: export TrxRecord

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { parseBCAStatement } from './parser/bca_parser';
 import { parseMandiriStatement } from './parser/mandiri_parser';
 import { parseMandiriCCStatement } from './parser/mandiri_cc_parser';
 
+export { TrxRecord } from './parser/shared';
+
 export {
     parseBCAStatement,
     parseMandiriCCStatement,
